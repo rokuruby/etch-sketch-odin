306dcb685559ea1f96c03e608fd9e4b35b1f5e2b
rokuruby <llltth@gmail.com>
2025-03-19 18:41:33 +0800

    made some pseudocode comments on the js file and added a div to house the reset button in html file

---
+++ logic.js
@@ -0,0 +1,11 @@
+//DOM Manipulation to form grids
+
+
+
+//Event to detect mouse click and change square to black color
+
+
+//Event to detect mouse click and revert square back to original color
+
+
+//event to detect button click to reset all grid to default color
